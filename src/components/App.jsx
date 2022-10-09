ed0102333f14c3359ea2ce868938bbbf0810972e
GansBRO27 <vladyslavgdjmg@gmail.com>
2022-10-10 02:16:56 +0300

    qwe

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,95 +4,153 @@ import ImageGallery from './imageGallery/imageGallery';
 import Form from './form/form';
 import LoadButton from './loadMoreBtn/loadBtn';
 import AppStyled from './app.styled';
-import Modal from './modal/modal';
+import ModalHook from './modal/modal';
 import { spiner } from './loader/loader';
 import fetchImages from 'services/fetchImg';
-
-export class App extends Component {
-  state = {
-    gallery: [],
-
-    showModal: false,
-    isLoading: false,
-    page: '1',
-    total: null,
-    largeImg: '',
-  };
-
-  onClose = () => {
-    this.setState({
-      showModal: false,
-    });
-  };
-  clickOnElem = url => {
-    this.setState({
-      showModal: !this.state.showModal,
-      largeImg: url,
-    });
-  };
-
-  handleSubmit = query => {
+import { useState } from 'react';
+import { useEffect } from 'react';
+export function AppHook() {
+  const [gallery, setGallery] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(null);
+  const [query, setQuery] = useState('');
+  const [largeImg, setLargeImg] = useState('');
+  useEffect(() => {
     if (query.trim() !== '') {
       setTimeout(() => {
-        fetchImages(query, this.state.page)
+        fetchImages(query, page)
           .then(responce => responce.json())
-          .then(({ hits, totalHits }) =>
-            this.setState({ gallery: hits, total: totalHits, page: '1' })
-          )
-          .finally(() => this.setState({ isLoading: false }));
+          .then(({ hits, totalHits }) => {
+            setGallery(prevState => [...prevState, ...hits]);
+            setTotal(totalHits);
+          })
+          .finally(() => setIsLoading(false));
       }, 1000);
     }
+  }, [query, page]);
+  const onClose = () => {
+    setShowModal(false);
   };
-
-  clickOnLoadMore = () => {
-    this.setState({
-      isLoading: true,
-    });
-    fetchImages(this.state.page)
-      .then(responce => responce.json())
-      .then(({ hits, totalHits }) =>
-        this.setState(prevState => {
-          return {
-            page: Number(prevState.page) + 1,
-            gallery: [...prevState.gallery, ...hits],
-            total: totalHits,
-          };
-        })
-      )
-      .finally(
-        this.setState({
-          isLoading: false,
-        })
-      );
+  const clickOnElem = url => {
+    setShowModal(!showModal);
+    setLargeImg(url);
   };
-  render() {
-    if (this.state.showModal) {
-      return (
-        <Modal
-          url={this.state.largeImg}
-          onClose={this.onClose}
-          onOverlayClick={this.clickOnOverlay}
-        />
-      );
+  const handleSubmit = query => {
+    if (query.trim() !== '') {
+      setQuery(query);
     }
-    return (
-      <AppStyled>
-        <SearchBar>
-          <Form onSubmit={this.handleSubmit} />
-        </SearchBar>
-        {!this.state.isLoading ? (
-          <ImageGallery
-            gallery={this.state.gallery}
-            onClick={this.clickOnElem}
-          />
-        ) : (
-          <span>{spiner}</span>
-        )}
+  };
+  const clickOnLoadMore = () => {
+    setPage(prevState => prevState + 1);
+  };
 
-        {this.state.gallery.length > 0 && !this.state.isLoading && (
-          <LoadButton onClick={this.clickOnLoadMore} />
-        )}
-      </AppStyled>
-    );
+  if (showModal) {
+    return <ModalHook url={largeImg} onClose={onClose} />;
   }
+  return (
+    <AppStyled>
+      <SearchBar>
+        <Form onSubmit={handleSubmit} />
+      </SearchBar>
+      {!isLoading ? (
+        <ImageGallery gallery={gallery} onClick={clickOnElem} />
+      ) : (
+        <span>{spiner}</span>
+      )}
+
+      {gallery.length > 0 && !isLoading && (
+        <LoadButton onClick={clickOnLoadMore} />
+      )}
+    </AppStyled>
+  );
 }
+// export class App extends Component {
+//   state = {
+//     gallery: [],
+
+//     showModal: false,
+//     isLoading: false,
+//     page: '1',
+//     total: null,
+//     largeImg: '',
+//   };
+
+//   onClose = () => {
+//     this.setState({
+//       showModal: false,
+//     });
+//   };
+//   clickOnElem = url => {
+//     this.setState({
+//       showModal: !this.state.showModal,
+//       largeImg: url,
+//     });
+//   };
+
+//   handleSubmit = query => {
+//     if (query.trim() !== '') {
+//       setTimeout(() => {
+//         fetchImages(query, this.state.page)
+//           .then(responce => responce.json())
+//           .then(({ hits, totalHits }) =>
+//             this.setState({ gallery: hits, total: totalHits, page: '1' })
+//           )
+//           .finally(() => this.setState({ isLoading: false }));
+//       }, 1000);
+//     }
+//   };
+
+//   clickOnLoadMore = () => {
+//     this.setState({
+//       isLoading: true,
+//     });
+//     fetchImages(this.state.page)
+//       .then(responce => responce.json())
+//       .then(({ hits, totalHits }) =>
+//         this.setState(prevState => {
+//           return {
+//             page: Number(prevState.page) + 1,
+//             gallery: [...prevState.gallery, ...hits],
+//             total: totalHits,
+//           };
+//         })
+//       )
+//       .finally(
+//         this.setState({
+//           isLoading: false,
+//         })
+//       );
+//   };
+//   render() {
+//     if (this.state.showModal) {
+//       return (
+//         <Modal
+//           url={this.state.largeImg}
+//           onClose={this.onClose}
+//           onOverlayClick={this.clickOnOverlay}
+//         />
+//       );
+//     }
+//     return (
+//       <AppStyled>
+//         <SearchBar>
+//           <Form onSubmit={this.handleSubmit} />
+//         </SearchBar>
+//         {!this.state.isLoading ? (
+//           <ImageGallery
+//             gallery={this.state.gallery}
+//             onClick={this.clickOnElem}
+//           />
+//         ) : (
+//           <span>{spiner}</span>
+//         )}
+
+//         {this.state.gallery.length > 0 && !this.state.isLoading && (
+//           <LoadButton onClick={this.clickOnLoadMore} />
+//         )}
+//       </AppStyled>
+//     );
+//   }
+// }
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -4,38 +4,75 @@ import Overlay from './modal.styled';
 import ModalStyled from './modalStyled.styled';
 import PropTypes from 'prop-types';
 import Img from './img.styled';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 const modalRoot = document.querySelector('#modal-root');
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.onHendlerEsc);
-    window.addEventListener('click', this.handleBackdropClick);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onHendlerEsc);
-    window.removeEventListener('click', this.handleBackdropClick);
-  }
-  handleBackdropClick = e => {
+
+export default function ModalHook({ url, onClose }) {
+  useEffect(() => {
+    const onHendlerEsc = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    const handleBackdropClick = e => {
+      if (e.target === e.currentTarget) {
+        onClose();
+      }
+    };
+    document.addEventListener('keydown', onHendlerEsc);
+    window.addEventListener('click', handleBackdropClick);
+    return () => {
+      document.removeEventListener('keydown', onHendlerEsc);
+      window.removeEventListener('click', handleBackdropClick);
+    };
+  }, []);
+  const handleBackdropClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
-  onHendlerEsc = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-  render() {
-    return createPortal(
-      <Overlay className="overlay" onClick={this.handleBackdropClick}>
-        <ModalStyled className="modal">
-          <Img src={this.props.url} alt="photo" />
-        </ModalStyled>
-      </Overlay>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <Overlay className="overlay" onClick={handleBackdropClick}>
+      <ModalStyled className="modal">
+        <Img src={url} alt="photo" />
+      </ModalStyled>
+    </Overlay>,
+    modalRoot
+  );
 }
-Modal.propTypes = {
+// export class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.onHendlerEsc);
+//     window.addEventListener('click', this.handleBackdropClick);
+//   }
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.onHendlerEsc);
+//     window.removeEventListener('click', this.handleBackdropClick);
+//   }
+//   handleBackdropClick = e => {
+//     if (e.target === e.currentTarget) {
+//       this.props.onClose();
+//     }
+//   };
+//   onHendlerEsc = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+//   render() {
+//     return createPortal(
+//       <Overlay className="overlay" onClick={this.handleBackdropClick}>
+//         <ModalStyled className="modal">
+//           <Img src={this.props.url} alt="photo" />
+//         </ModalStyled>
+//       </Overlay>,
+//       modalRoot
+//     );
+//   }
+// }
+ModalHook.propTypes = {
   onElemClick: PropTypes.func,
   onOverlayClick: PropTypes.func,
   url: PropTypes.string,
